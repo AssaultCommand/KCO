94b1d256d80abe556a58ba54681dc13469c011c8
Neervoort,Caspar C.P.W <295720@student.fontys.nl>
2015-04-28 15:29:21 +0200

    Set file structure & app structure
    Load pages into index.php at the include in <section class="view">
    
    Dynamic includes via get variable will come later.

---
+++ webapp/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>	
+	<meta charset="UTF-8">
+</head>
+
+<body>
+	
+	<h1>Amsterdam Audio Tour</h1>
+	<p>Stuff will be added here soon<sup>tm</sup>.</p>
+</body>	
+
+</html>
