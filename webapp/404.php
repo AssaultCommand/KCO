0db33e21334a9cf2b7238128e5f4bff6cd5c15d1
Neervoort,Caspar C.P.W <295720@student.fontys.nl>
2015-04-29 11:36:32 +0200

    Style fixes, 404 page added
    Fixed the view element, the header and the nav bar and added a 404 page

---
+++ webapp/404.php
@@ -0,0 +1,6 @@
+<h2 class="errortitle">404</h2>
+<p class="errortext">
+	It looks like you're trying to access something that doesn't exist.<br>
+	<br>
+	Try coming here at a later time if you believe this page should exist as it may just be down temporarily.
+</p>
