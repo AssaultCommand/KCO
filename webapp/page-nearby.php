b30ea0f57b54584f43a8d92a11652a92806534cf
Diane Melaan <laciepark300323@hotmail.com>
2015-04-29 11:56:43 +0200

    Start on nearby page

---
+++ webapp/page-nearby.php
@@ -0,0 +1,10 @@
+<section class="card">
+		<h2 class="nearbytitle">Nearby Sights</h2>
+		<h3 class="distance">Distance: 5 KM</h3>
+</section>
+
+<article class="card nearby">
+	<div class="nearbyimage"><img src="image/.png" alt=""></div>
+		<h2 class="nearbytitle">Royal Concertgebouw Orchestra</h2>
+		<h3 class="distance">Distance: 0 KM</h3>
+</article>
