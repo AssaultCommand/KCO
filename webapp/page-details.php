3fea725f22b0f3da6f2679364fce032077084650
Diane Melaan <laciepark300323@hotmail.com>
2015-04-29 12:33:04 +0200

    details page + css changes

---
+++ webapp/page-details.php
@@ -0,0 +1,15 @@
+<section class="card">
+  		<h2 class="detailstitle">Royal Concertgebouw Orchestra</h2>
+		
+		<div class="detailsimage"></div>
+		<div class="togglebutton"></div>
+		<div class="mapbutton"></div>
+		
+		<p class="detailsdescription">
+			The Concertgebouw opened on 11 April 1888. The Concertgebouw
+			Orchestra was established several months later, and gave its first
+			concert in the Concertgebouw on 3 november 1888, conducted by the orchestra's
+			first chief conducter, Willem Kes.
+			
+		</p>
+    </section>
