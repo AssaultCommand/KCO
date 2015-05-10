dc870bf14c250afecbf52a8f2805e1fca9a9aee4
Diane Melaan <laciepark300323@hotmail.com>
2015-05-10 23:28:05 +0200

    Nearby page code
    Casper please fix the text aligning. I cant do it T T

--- webapp/page-nearby.php
+++ webapp/page-nearby.php
@@ -1,10 +1,10 @@
-<section class="card">
-		<h2 class="nearbytitle">Nearby Sights</h2>
-		<h3 class="distance">Distance: 5 KM</h3>
-</section>
+	<section class="card">
+			<h2 class="nearby_sights">Nearby Sights</h2>
+			<h3 class="distancenear">Distance: 5 KM</h3>
+	</section>
 
-<article class="card nearby">
-	<div class="nearbyimage"><img src="image/.png" alt=""></div>
-		<h2 class="nearbytitle">Royal Concertgebouw Orchestra</h2>
-		<h3 class="distance">Distance: 0 KM</h3>
-</article>
+	<article class="card nearby">
+		<div class="nearbyimage"></div>
+			<h2 class="nearbytitle">Royal Concertgebouw Orchestra</h2>
+			<h3 class="distancenearby">Distance: 0 KM</h3>
+	</article>
