bab31e9c39488ef11477e4cdfcc0211648c6ced0
AssaulltCommand <beststealthsniper@live.com>
2015-05-11 10:53:47 +0200

    Details Page & Nearby Page Fixes

--- webapp/page-details.php
+++ webapp/page-details.php
@@ -7,17 +7,19 @@
 			<div class="togglebuttons">
 			  <div class="addbutton togglebutton"></div>
 			  <div class="mapbutton togglebutton"></div>
-			  <div class="distance">1.5 KM</div>
+			  <div class="detailsDistance">0.5 KM</div>
 			</div>
 		</div>
 		
-		<div class="soundbutton"></div>
+		<div class="soundButton"></div>
+		<div class="soundProgress">
+			<div class="soundProgressHandlebar"></div>
+		</div>
 		
 		<p class="detailsdescription">
 			The Concertgebouw opened on 11 April 1888. The Concertgebouw
 			Orchestra was established several months later, and gave its first
 			concert in the Concertgebouw on 3 november 1888, conducted by the orchestra's
 			first chief conducter, Willem Kes.
-			
 		</p>
 	</section>
--- webapp/page-nearby.php
+++ webapp/page-nearby.php
@@ -1,42 +1,52 @@
 	<section class="card nearbySightsCard">
-			<h2 class="cardTitle">Nearby Sights</h2>
-			<h3 class="maximumDistance">Maximum distance: <span class="maximumDistanceValue">5 KM</span></h3>
-			<div class="maximumDistanceSlider">
-				<div class="maximumDistanceHandlebar"></div>
-			</div>
+		<h2 class="cardTitle">Nearby Sights</h2>
+		<h3 class="maximumDistance">Maximum distance: <span class="maximumDistanceValue">5 KM</span></h3>
+		<div class="maximumDistanceSlider">
+			<div class="maximumDistanceHandlebar"></div>
+		</div>
 	</section>
 
-	<article class="card nearbyCard">
-		<div class="nearbyimage" style="background-image: url(images/sights/concertgebouw.jpg);"></div>
-		<h2 class="nearbytitle">Royal Concertgebouw Orchestra</h2>
-		<h3 class="distancenearby">Distance: <strong>0.5 KM</strong></h3>
-		<div class="nearbyPopupMenu"></div>
-	</article>
+	<a href="details-1" class="cardButton">
+		<article class="card nearbyCard">
+			<div class="nearbyimage" style="background-image: url(images/sights/concertgebouw.jpg);"></div>
+			<h2 class="nearbytitle">Royal Concertgebouw Orchestra</h2>
+			<h3 class="distancenearby">Distance: <strong>0.5 KM</strong></h3>
+			<div class="nearbyPopupMenu"></div>
+		</article>
+	</a>
 
-	<article class="card nearbyCard">
-		<div class="nearbyimage" style="background-image: url(images/sights/vangogh.jpg);"></div>
-		<h2 class="nearbytitle">Van Gogh Museum</h2>
-		<h3 class="distancenearby">Distance: <strong>0.8 KM</strong></h3>
-		<div class="nearbyPopupMenu"></div>
-	</article>
+	<a href="details-2" class="cardButton">
+		<article class="card nearbyCard">
+			<div class="nearbyimage" style="background-image: url(images/sights/vangogh.jpg);"></div>
+			<h2 class="nearbytitle">Van Gogh Museum</h2>
+			<h3 class="distancenearby">Distance: <strong>0.8 KM</strong></h3>
+			<div class="nearbyPopupMenu"></div>
+		</article>
+	</a>
 
-	<article class="card nearbyCard">
-		<div class="nearbyimage" style="background-image: url(images/sights/stedelijkmuseum.jpg);"></div>
-		<h2 class="nearbytitle">Stedelijk Museum Amsterdam</h2>
-		<h3 class="distancenearby">Distance: <strong>0.9 KM</strong></h3>
-		<div class="nearbyPopupMenu"></div>
-	</article>
+	<a href="details-3" class="cardButton">
+		<article class="card nearbyCard">
+			<div class="nearbyimage" style="background-image: url(images/sights/stedelijkmuseum.jpg);"></div>
+			<h2 class="nearbytitle">Stedelijk Museum Amsterdam</h2>
+			<h3 class="distancenearby">Distance: <strong>0.9 KM</strong></h3>
+			<div class="nearbyPopupMenu"></div>
+		</article>
+	</a>
 
-	<article class="card nearbyCard">
-		<div class="nearbyimage" style="background-image: url(images/sights/rijksmuseum.jpg);"></div>
-		<h2 class="nearbytitle">Rijksmuseum</h2>
-		<h3 class="distancenearby">Distance: <strong>2.2 KM</strong></h3>
-		<div class="nearbyPopupMenu"></div>
-	</article>
+	<a href="details-4" class="cardButton">
+		<article class="card nearbyCard">
+			<div class="nearbyimage" style="background-image: url(images/sights/rijksmuseum.jpg);"></div>
+			<h2 class="nearbytitle">Rijksmuseum</h2>
+			<h3 class="distancenearby">Distance: <strong>2.2 KM</strong></h3>
+			<div class="nearbyPopupMenu"></div>
+		</article>
+	</a>
 
-	<article class="card nearbyCard">
-		<div class="nearbyimage" style="background-image: url(images/sights/annefrank.jpg);"></div>
-		<h2 class="nearbytitle">Anne Frank House</h2>
-		<h3 class="distancenearby">Distance: <strong>4.3 KM</strong></h3>
-		<div class="nearbyPopupMenu"></div>
-	</article>
+	<a href="details-5" class="cardButton">
+		<article class="card nearbyCard">
+			<div class="nearbyimage" style="background-image: url(images/sights/annefrank.jpg);"></div>
+			<h2 class="nearbytitle">Anne Frank House</h2>
+			<h3 class="distancenearby">Distance: <strong>4.3 KM</strong></h3>
+			<div class="nearbyPopupMenu"></div>
+		</article>
+	</a>
